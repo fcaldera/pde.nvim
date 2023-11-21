a0818d7497b749fb77c3b50e2ec6d76fdf882de4
Fernando Caldera <fer.caldera@gmail.com>
2023-11-21 09:39:30 +0200

    Add tree switcher and fold mappings

--- lua/fernando/options.lua
+++ lua/fernando/options.lua
@@ -9,12 +9,12 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -27,25 +27,25 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
 -- tabs & indentation
-vim.opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 2    -- 2 spaces for indent width
-vim.opt.expandtab = true  -- expand tab to spaces
+vim.opt.shiftwidth = 2 -- 2 spaces for indent width
+vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
 vim.opt.smartindent = true
-vim.opt.colorcolumn = '80'
+-- vim.opt.colorcolumn = '80'
 
 -- vim: ts=2 sts=2 sw=2 et
