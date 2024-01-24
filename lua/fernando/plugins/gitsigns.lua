a317657003218ebc659e743e5d2db77f8b77a5f1
Fernando Caldera <fer.caldera@gmail.com>
2024-01-24 15:34:09 +0200

    Add git blame heymaps

--- lua/fernando/plugins/gitsigns.lua
+++ lua/fernando/plugins/gitsigns.lua
@@ -11,16 +11,17 @@ return {
       changedelete = { text = '~' },
     },
     on_attach = function(bufnr)
-      vim.keymap.set(
-        'n', '<leader>hp',
-        require('gitsigns').preview_hunk,
-        { buffer = bufnr, desc = 'Preview git hunk' }
-      )
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
 
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
 
-      vim.keymap.set({ 'n', 'v' }, ']c', function()
+      map({ 'n', 'v' }, ']c', function()
         if vim.wo.diff then
           return ']c'
         end
@@ -28,9 +29,9 @@ return {
           gs.next_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+      end, { expr = true, desc = 'Jump to next hunk' })
 
-      vim.keymap.set({ 'n', 'v' }, '[c', function()
+      map({ 'n', 'v' }, '[c', function()
         if vim.wo.diff then
           return '[c'
         end
@@ -38,7 +39,21 @@ return {
           gs.prev_hunk()
         end)
         return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+      end, { expr = true, desc = 'Jump to previous hunk' })
+
+      -- map('n', '<leader>hs', gs.stage_hunk)
+      -- map('n', '<leader>hr', gs.reset_hunk)
+      -- map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      -- map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      -- map('n', '<leader>hS', gs.stage_buffer)
+      -- map('n', '<leader>hu', gs.undo_stage_hunk)
+      -- map('n', '<leader>hR', gs.reset_buffer)
+      map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview git hunk" })
+      map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = "Blame line" })
+      map('n', '<leader>hB', gs.toggle_current_line_blame, { desc = "Toogle current line blame" })
+      -- map('n', '<leader>hd', gs.diffthis)
+      -- map('n', '<leader>hD', function() gs.diffthis('~') end)
+      -- map('n', '<leader>td', gs.toggle_deleted)
     end,
   },
 }
