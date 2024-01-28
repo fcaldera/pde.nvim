89d52639ffd39490c68d0479b86e2dff059a0a49
Fernando Caldera <fer.caldera@gmail.com>
2024-01-28 20:03:40 +0200

    Configure netrw for a better experience

--- lua/fernando/options.lua
+++ lua/fernando/options.lua
@@ -53,4 +53,8 @@ vim.opt.wrap = false
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
+-- File explorer
+vim.g.netrw_preview = 1 -- Preview with vertical split 
+vim.g.netrw_keepdir = 0 -- keep the current dir the same as the browsing dir
+
 -- vim: ts=2 sts=2 sw=2 et
