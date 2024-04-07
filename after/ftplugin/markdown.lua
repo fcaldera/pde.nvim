b8cbcd2771d97a153acb28e0c2d17d7186ee5f17
Fernando Caldera <fer.caldera@gmail.com>
2024-04-07 18:38:33 +0300

    Add elixir tools

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
-vim.opt.wrap = true
--- vim.opt.textwidth = 80
-vim.opt.spelllang = "en_us"
-vim.opt.spell = true
+local set = vim.opt_local
+
+set.wrap = true
+set.spelllang = "en_us"
+set.spell = true
+-- set.textwidth = 80
